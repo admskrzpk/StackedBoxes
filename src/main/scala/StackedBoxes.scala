d626e6ac9b133d8d110cf25e1c6435df2b3922ea
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-03-23 13:38:03 +0100

    Update StackedBoxes.scala

--- src/main/scala/StackedBoxes.scala
+++ src/main/scala/StackedBoxes.scala
@@ -5,7 +5,6 @@ val width = names(2).length
 
 names.foreach{ name  =>
     println( " " * ((width - name.length) / 2) + "+" + "-" * name.length + "+")
-    // |name|
     println( " " * ((width - name.length) / 2) + s"|$name|")
 
     if (names.last == name) println( " " * ((width - name.length) / 2) + "+" + "-" * name.length + "+")
