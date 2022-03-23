6a7994dd3f41be4aacc8e2abea667dc2333919cf
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-03-23 13:38:03 +0100

    Update StackedBoxes.scala

--- src/main/scala/StackedBoxes.scala
+++ src/main/scala/StackedBoxes.scala
@@ -1,11 +1,10 @@
 object StackedBoxes extends App {
 
-val names = args
-val width = names(2).length
+val names = args.sortBy(x => (x.length, x.head))
+val width = names.last.length
 
 names.foreach{ name  =>
     println( " " * ((width - name.length) / 2) + "+" + "-" * name.length + "+")
-    // |name|
     println( " " * ((width - name.length) / 2) + s"|$name|")
 
     if (names.last == name) println( " " * ((width - name.length) / 2) + "+" + "-" * name.length + "+")
